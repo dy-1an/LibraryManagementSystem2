18c2b7c95d941e0a33382811ca7e3f79021fb1b1
ADMIN <ADMIN@DESKTOP-R7HH38E>
2024-07-11 11:31:50 +0300

    Add project files.

---
+++ Management/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtYearPublished = new System.Windows.Forms.TextBox();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.dvgBooks = new System.Windows.Forms.DataGridView();
+            this.masterDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.masterDataSet = new Management.masterDataSet();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgBooks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Author";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Year Published";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Genre";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(193, 39);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(161, 20);
+            this.txtTitle.TabIndex = 4;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(193, 88);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(161, 20);
+            this.txtAuthor.TabIndex = 5;
+            // 
+            // txtYearPublished
+            // 
+            this.txtYearPublished.Location = new System.Drawing.Point(193, 131);
+            this.txtYearPublished.Name = "txtYearPublished";
+            this.txtYearPublished.Size = new System.Drawing.Size(161, 20);
+            this.txtYearPublished.TabIndex = 6;
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Location = new System.Drawing.Point(193, 183);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(161, 20);
+            this.txtGenre.TabIndex = 7;
+            // 
+            // dvgBooks
+            // 
+            this.dvgBooks.AllowUserToAddRows = false;
+            this.dvgBooks.AllowUserToDeleteRows = false;
+            this.dvgBooks.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dvgBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgBooks.Location = new System.Drawing.Point(396, 39);
+            this.dvgBooks.Name = "dvgBooks";
+            this.dvgBooks.ReadOnly = true;
+            this.dvgBooks.Size = new System.Drawing.Size(392, 164);
+            this.dvgBooks.TabIndex = 8;
+            this.dvgBooks.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dvgBooks.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvgBooks_CellContentClick);
+            // 
+            // masterDataSetBindingSource
+            // 
+            this.masterDataSetBindingSource.DataSource = this.masterDataSet;
+            this.masterDataSetBindingSource.Position = 0;
+            // 
+            // masterDataSet
+            // 
+            this.masterDataSet.DataSetName = "masterDataSet";
+            this.masterDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(20, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 29);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(166, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 29);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(302, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 29);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(468, 261);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 29);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dvgBooks);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.txtYearPublished);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvgBooks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void dvgBooks_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtYearPublished;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.DataGridView dvgBooks;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.BindingSource masterDataSetBindingSource;
+        private masterDataSet masterDataSet;
+    }
+}
+
---
+++ Management/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Management
+{
+    public partial class Form1 : Form
+    {
+        DataTable table=new DataTable("table");
+        int index;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.Columns.Add("Title", Type.GetType("System.String"));
+            table.Columns.Add("Author", Type.GetType("System.String"));
+            table.Columns.Add("Year Published", Type.GetType("System.Int32"));
+            table.Columns.Add("Genre", Type.GetType("System.String"));
+            dvgBooks.DataSource = table;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-R7HH38E;Initial Catalog=LMSDB;Integrated Security=True;");
+            con.Open();
+            table.Rows.Add(txtTitle.Text,txtAuthor.Text,txtYearPublished.Text,txtGenre.Text);
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-R7HH38E;Initial Catalog=LMSDB;Integrated Security=True;");
+            con.Open();
+            txtTitle.Text = string.Empty;
+            txtAuthor.Text=string.Empty;
+            txtYearPublished.Text=string.Empty;
+            txtGenre.Text=string.Empty;
+            con.Close();
+        }
+
+        private void dataGridview1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            index= e.RowIndex;
+            DataGridViewRow row = dvgBooks.Rows[index];
+            txtTitle.Text = row.Cells[0].Value.ToString();
+            txtAuthor.Text = row.Cells[1].Value.ToString();
+            txtYearPublished.Text = row.Cells[2].Value.ToString();
+            txtGenre.Text = row.Cells[3].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-R7HH38E;Initial Catalog=LMSDB;Integrated Security=True;");
+            con.Open();
+            DataGridViewRow newdata=dvgBooks.Rows[index];
+            newdata.Cells[0].Value = txtTitle.Text;
+            newdata.Cells[1].Value = txtAuthor.Text;
+            newdata.Cells[2].Value = txtYearPublished.Text;
+            newdata.Cells[3].Value = txtGenre.Text;
+            con.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-R7HH38E;Initial Catalog=LMSDB;Integrated Security=True;");
+            con.Open();
+            index =dvgBooks.CurrentCell.RowIndex;
+            dvgBooks.Rows.RemoveAt(index);
+            con.Close();
+            DataTable dt = new DataTable();
+            
+            dvgBooks.DataSource = dt;
+        }
+
+        
+    }
+}
